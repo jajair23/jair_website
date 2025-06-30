bf057cf4411e5fa94769c629723c428b0ce2a943
Jair Orduña <60374904+jajair23@users.noreply.github.com>
2025-07-01 00:53:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Javascript for hover cards
+
+document.getElementById("hover_cards").onmousemove = e => {
+  for(const card of document.getElementsByClassName("card")) {
+    const rect = card.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  };
+}
+
+// hover card ends here
